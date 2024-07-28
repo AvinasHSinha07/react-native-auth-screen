ac8465f7906c4c012d7a69b4a7e64dc7180ffb74
Avinash <avinashsinha751@gmail.com>
2024-07-29 02:50:17 +0600

    changes made

--- screens/InitialScreen.js
+++ screens/InitialScreen.js
@@ -1,110 +1,135 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
 
 export default function InitialScreen({ navigation }) {
   return (
-    <ImageBackground source={require('../assets/allef-vinicius-IvQeAVeJULw-unsplash(1).jpg')} style={styles.container}>
-      <View style={styles.overlay}>
-        <View style={styles.header}>
-          <Text style={styles.title}>INBARBER</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <ImageBackground
+        source={require("../assets/allef-vinicius-IvQeAVeJULw-unsplash(1).jpg")}
+        style={styles.container}
+      >
+        <View style={styles.overlay}>
+          <View style={styles.header}>
+            <Text style={styles.title}>INBARBER</Text>
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.subtitle}>Bem-vindo a agenda feita para você</Text>
+            <Text style={styles.description}>
+              Planeje e organize seu dia a dia, alcance seus objetivos e
+              transforme seu futuro.
+            </Text>
+          </View>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate("Login")}
+            >
+              <Text style={styles.buttonText}>ENTRAR</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.secondaryButton]}
+              onPress={() => navigation.navigate("Signup")}
+            >
+              <Text style={[styles.buttonText, styles.secondaryButtonText]}>
+                CADASTRAR
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>
+              Termos e Condições | Política de Privacidade
+            </Text>
+          </View>
         </View>
-        <View style={styles.content}>
-          <Text style={styles.subtitle}>Bem-vindo a agenda feita para você</Text>
-          <Text style={styles.description}>
-            Planeje e organize seu dia a dia, alcance seus objetivos e transforme seu futuro.
-          </Text>
-        </View>
-        <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
-            <Text style={styles.buttonText}>ENTRAR</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.secondaryButton]} onPress={() => navigation.navigate('Signup')}>
-            <Text style={styles.buttonText}>CADASTRAR</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.footer}>
-          <Text style={styles.footerText}>Termos e Condições | Política de Privacidade</Text>
-        </View>
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
     flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center",
     padding: 20,
   },
   header: {
-    flex: 1,
-    justifyContent: 'center',
+    position: "absolute",
+    top: 250,
+    alignItems: "center",
+    width: "100%",
   },
   title: {
-    fontSize: 36,
-    fontWeight: 'bold',
-    color: 'white',
-    marginTop: 90,
+    fontSize: 48,
+    fontWeight: "bold",
+    color: "white",
   },
   content: {
-    flex: 1,
-    
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    marginTop: 90,
+    width: "100%",
   },
   subtitle: {
-    fontSize: 30,
-    color: 'white',
-    fontWeight:'700',
-    textAlign: 'left',
+    fontSize: 24,
+    color: "white",
+    fontWeight: "700",
+    textAlign: "left",
     marginBottom: 10,
-    
   },
   description: {
-    fontSize: 12,
-    color: 'white',
-    textAlign: 'left',
-
-    
+    fontSize: 16,
+    color: "white",
+    textAlign: "left",
+    marginBottom: 30,
+    // paddingHorizontal: 20,
   },
   buttonsContainer: {
-    flex: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 40,
   },
   button: {
-    backgroundColor: '#c87f52',
+    backgroundColor: "#c87f52",
     paddingVertical: 15,
-    borderRadius: 15,
+    borderRadius: 50,
     marginVertical: 10,
-    width: '80%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   secondaryButton: {
-    backgroundColor: 'transparent',
-    borderColor: "white",
+    backgroundColor: "transparent",
+    borderColor: "rgba(255, 255, 255, 0.5)",
     borderWidth: 1,
   },
+  secondaryButtonText: {
+    color: "white",
+  },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
   },
   footer: {
-    justifyContent: 'flex-end',
+    width: "100%",
     paddingBottom: 20,
+    alignItems: "center",
   },
   footerText: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     fontSize: 12,
   },
 });
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,119 +1,124 @@
-// screens/LoginScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ImageBackground, Image, SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
 
-export default function LoginScreen({ navigation }) {
+export default function LoginScreen() {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const navigation = useNavigation();
 
   return (
-    <ImageBackground source={require('../assets/allef-vinicius-IvQeAVeJULw-unsplash(1).jpg')} style={styles.container}>
-      <View style={styles.overlay}>
-        <View style={styles.header}>
-          <Image
-            source={require('../assets/7057474_592-removebg-preview.png')} // Replace with your logo path
-            style={styles.logo}
-            resizeMode="contain"
-          />
-          {/* <Text style={styles.title}>INBARBER</Text> */}
-        </View>
-        <View style={styles.formContainer}>
-          <TextInput
-            placeholder="E-mail de acesso"
-            style={styles.input}
-            placeholderTextColor="#ccc"
-          />
-          <View style={styles.passwordContainer}>
+    <SafeAreaView style={styles.safeArea}>
+      <ImageBackground source={require('../assets/allef-vinicius-IvQeAVeJULw-unsplash(1).jpg')} style={styles.container}>
+        <View style={styles.overlay}>
+          <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+            <FontAwesome name="arrow-left" size={24} color="white" />
+          </TouchableOpacity>
+          <View style={styles.header}>
+            <Text style={styles.title}>INBARBER</Text>
+          </View>
+          <View style={styles.formContainer}>
+            <Text style={styles.welcomeText}>
+              Bem-vindo parceiro, entre com seus dados para ter acesso a sua agenda
+            </Text>
             <TextInput
-              placeholder="Sua senha"
-              style={styles.passwordInput}
-              placeholderTextColor="#ccc"
-              secureTextEntry={!showPassword}
-              value={password}
-              onChangeText={setPassword}
+              placeholder="E-mail de acesso"
+              style={styles.input}
+              placeholderTextColor="#ffffff"
             />
-            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-              <Icon
-                name={showPassword ? "visibility" : "visibility-off"}
-                size={24}
-                color="#ccc"
+            <View style={styles.passwordContainer}>
+              <TextInput
+                placeholder="Sua senha"
+                style={styles.passwordInput}
+                placeholderTextColor="#ffffff"
+                secureTextEntry={!showPassword}
+                value={password}
+                onChangeText={setPassword}
               />
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <Icon
+                  name={showPassword ? "visibility" : "visibility-off"}
+                  size={24}
+                  color="#ccc"
+                />
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={() => { /* handle login */ }}>
+              <Text style={styles.buttonText}>ENVIAR</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { /* handle password reset */ }}>
+              <Text style={styles.link}>Esqueceu sua senha?</Text>
             </TouchableOpacity>
           </View>
-          <TouchableOpacity style={styles.button} onPress={() => { /* handle login */ }}>
-            <Text style={styles.buttonText}>ENVIAR</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => { /* handle password reset */ }}>
-            <Text style={styles.link}>Esqueceu sua senha?</Text>
-          </TouchableOpacity>
+          <Text style={styles.footerText}>
+            Sua agenda possui dados sensíveis de seus clientes, não forneça seu acesso a ninguém.
+          </Text>
         </View>
-        <View style={styles.socialContainer}>
-          {/* <Text style={styles.socialText}>Ou entre com</Text> */}
-          <View style={styles.socialIcons}>
-            <TouchableOpacity style={styles.socialButton}>
-              <FontAwesome name="facebook" size={30} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.socialButton}>
-              <FontAwesome name="instagram" size={30} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.socialButton}>
-              <FontAwesome name="twitter" size={30} color="white" />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end', // Align content towards the bottom
   },
   overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)', // Darker overlay for better contrast
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end', // Align content towards the bottom
     alignItems: 'center',
     padding: 20,
   },
-  header: {
-    marginBottom: 0,
-    alignItems: 'center',
+  backButton: {
+    position: 'absolute',
+    top: 40,
+    left: 20,
   },
-  logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
+  header: {
+    alignItems: 'center',
+    marginBottom: 20,
   },
   title: {
-    fontSize: 36,
-    fontWeight: 'bold',
     color: 'white',
-    textAlign: 'center',
+    fontSize: 48,
+    fontWeight: 'bold',
   },
   formContainer: {
     width: '100%',
-    marginTop: 50,
     alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)', // Increased opacity for better readability
+    padding: 20,
+    borderRadius: 15,
+    marginBottom: 0, 
+  },
+  welcomeText: {
+    color: '#ccc',
+    marginBottom: 20,
+    textAlign: 'center',
   },
   input: {
-    width: '80%',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    width: '100%',
+    borderColor: "rgba(255, 255, 255, 0.5)",
+    borderWidth: 1,
     padding: 15,
-    borderRadius: 15,
+    borderRadius: 50,
     marginVertical: 10,
     color: 'white',
   },
   passwordContainer: {
     flexDirection: 'row',
-    width: '80%',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    width: '100%',
+    borderColor: "rgba(255, 255, 255, 0.5)",
+    borderWidth: 1,
     padding: 15,
-    borderRadius: 15,
+    borderRadius: 50,
     marginVertical: 10,
     alignItems: 'center',
   },
@@ -124,9 +129,9 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#c87f52',
     paddingVertical: 15,
-    borderRadius: 15,
+    borderRadius: 50,
     marginVertical: 10,
-    width: '80%',
+    width: '100%',
     alignItems: 'center',
   },
   buttonText: {
@@ -135,31 +140,14 @@ const styles = StyleSheet.create({
   },
   link: {
     color: '#ccc',
-    marginTop: 20,
+    marginTop: 10,
     textAlign: 'center',
   },
-  socialContainer: {
-    marginTop: 40,
-    alignItems: 'center',
-  },
-  socialText: {
-    color: '#ccc',
-    marginBottom: 10,
-    fontSize: 16,
-  },
-  socialIcons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-   width: "50%"
-  },
-  socialButton: {
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    // padding: 10,
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+  footerText: {
+    color: '#f4f4f4',
+    marginBottom: 20, // Added margin for spacing from the form
+    textAlign: 'center',
+    paddingHorizontal: 20,
   },
 });
+
